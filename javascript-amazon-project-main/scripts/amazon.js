3716540151a415c2118b570de1474fc95e8a6fd0
Dennis <duahdennis33@gmail.com>
2024-07-22 17:54:08 -0700

    Make add to cart interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,35 +1,11 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents:2095
-  }, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 799
-    }
-  }]; 
-
 let productsHTML = '';
 
 products.forEach((products) => {
   productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
-          <img class="product-image" src="${products.image}">
+          <img class="product-image"
+          src="${products.image}">
         </div>
 
         <div class="product-name limit-text-to-2-lines">
@@ -63,70 +39,46 @@ products.forEach((products) => {
         </div>
 
         <div class="product-spacer"></div>
-
         <div class="added-to-cart">
           <img src="images/icons/checkmark.png">
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary
+        js-add-to-cart"
+        data-product-id="${products.name}">
           Add to Cart
         </button>
       </div>
-
-      <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image" src="images/products/intermediate-composite-basketball.jpg">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          Intermediate Size Basketball
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-40.png">
-          <div class="product-rating-count link-primary">
-            127
-          </div>
-        </div>
-
-        <div class="product-price">
-          $20.95
-        </div>
-
-        <div class="product-quantity-container">
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
-        <div class="product-spacer"></div>
-
-        <div class="added-to-cart">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
-
-        <button class="add-to-cart-button button-primary">
-          Add to Cart
-        </button>
-      </div>
-
-  `
-
+  `;
 });
 
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-  console.log(productsHTML);
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productsId = button.dataset.productsId;
 
-document.querySelector('.js-products-grid').
-  innerHTML = productsHTML;
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productsId === item.productsId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      }
+      else {
+        cart.push({
+          productsId: productsId,
+          quantity: 1
+        });
+      }
+
+
+      console.log(cart);
+    });
+  });
