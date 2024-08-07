5db84689c9a5650e10f74bbc37599b4d4db4145b
Dennis <duahdennis33@gmail.com>
2024-08-07 02:08:34 -0700

    save cart in local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,12 +1,20 @@
-export let cart = [{
-  productsId:
-    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 3,
-}, {
-  productsId:
-    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 2,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  [{
+    productsId:
+      'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 3,
+  }, {
+    productsId:
+      '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 2,
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+  }
 
 export function addToCart(productsId) {
   let matchingItem;
@@ -26,6 +34,8 @@ export function addToCart(productsId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,5 +47,7 @@ export function removeFromCart(productId) {
     }
   });
 
+
+  saveToStorage();
   cart = newCart;
 }
