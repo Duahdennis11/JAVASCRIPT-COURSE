d2ff1e9157b9d489b0a57cce9935b97e6372677d
Dennis <duahdennis33@gmail.com>
2024-08-11 06:39:48 -0700

    save changes

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -4,11 +4,11 @@ if (!cart) {
   [{
     productsId:
       'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 3,
+    quantity: 2,
   }, {
     productsId:
       '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 2,
+    quantity: 1,
   }];
 }
 
@@ -28,7 +28,7 @@ export function addToCart(productsId) {
   if (matchingItem) {
     matchingItem.quantity += 1;
   }
-  
+
   else {
     cart.push({
       productsId: productsId,
