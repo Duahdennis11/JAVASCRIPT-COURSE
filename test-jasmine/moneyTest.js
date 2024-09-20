d2d04e37effc1720476e56bec36d2d1667091e94
Dennis <duahdennis33@gmail.com>
2024-09-20 03:06:39 -0700

    hhh

--- test-jasmine/moneyTest.js
+++
@@ -1,7 +0,0 @@
-import { formatCurrency } from '../';
-
-describe('test suite: formatCurrency', () => {
-  it('Convert cents into dollars', () => {
-    expect(formatCurrency(2095).toEqual('20.95'))
-  });
-});
